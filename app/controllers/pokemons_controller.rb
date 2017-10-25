241fa80d4141cfbd06bf71ea8e484fca6c11572c
Serra Park <serra.park@berkeley.edu>
2017-10-25 00:23:14 -0700

    finish up to part 5

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,40 @@
+class PokemonsController < ApplicationController
+  def capture
+    pokemon = Pokemon.find(params[:id])
+    pokemon.update(trainer_id: current_trainer.id)
+    pokemon.save
+    redirect_to root_path
+  end
+
+  def damage
+    pokemon = Pokemon.find(params[:id])
+    new_health = pokemon.health - 10
+    pokemon.update(health:new_health)
+    if pokemon.health <= 0
+      pokemon.destroy
+    end
+    redirect_to train_path(pokemon.trainer.id)
+  end
+
+  def new
+    @pokemon = Pokemon.new
+  end
+
+  def create
+    pokemon = Pokemon.new
+    pokemon.trainer_id = current_trainer.id
+    pokemon.health = 100
+    pokemon.level = 1
+    pokemon.update(poke_params)
+    pokemon.save
+    if pokemon.persisted?
+      redirect_to train_path(pokemon.trainer_id)
+    else
+      redirect_to pokemon_new_path, :flash => { :error => "Name cannot be blank or duplicate!" }
+    end
+  end
+
+  def poke_params
+    params.require(:pokemon).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :trainers
-  root to: "home#index"
   resources :trainers
+  root to: "home#index"
+  patch "capture", to: "pokemons#capture", as: "capture"
+  patch "damage", to: "pokemons#damage", as: "damage"
+  get "trainers/:id", to: "trainers#show", as: "train"
+  get "pokemons", to: "pokemons#new", as: "pokemon_new"
+  post "pokemons", to: "pokemons#create", as: "pokemon_create"
 end
